6a5a5f24f838795e6662cf397c8022888ce6c841
hamdiE <iragon158@gmail.com>
2022-01-15 15:45:29 +0100

    dddd

--- database.js
+++ database.js
@@ -3,14 +3,15 @@ const personSchema = require('./person.js');
 require('dotenv').config();
 const url = process.env.ATLAS_URI;
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
-    console.log('Database connection successful')
+    console.log('Database connection successful');
 }).catch(err => {
-    console.error('Database connection error')
-})/*
+    console.error('Database connection error');
+});
 const person = new personSchema({
     name: "afef",
     age: 42,
-})
+});
+
 person.save(function (err, data) {
     console.log(data);
 })
@@ -21,13 +22,12 @@ personSchema.create([
     },
     {
         name: "chiheb",
-        age:48
+        age: 48
     }
-]).then((d) => console.log(d))*/
-personSchema.find().then(d =>{
-    console.log('find',d);
- })
- personSchema.findById("61e19e0710178aa0757c5425").then(d =>{
-     console.log('find by id',d);
- })
- 
+]).then((d) => console.log(d));
+personSchema.find().then(d => {
+    console.log('find', d);
+})
+personSchema.findById("61e19e0710178aa0757c5425").then(d => {
+    console.log('find by id', d);
+})
